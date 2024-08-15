3255fde4568537f3360454a3b742a245c249fc51
Dominic Roberge <dominic.roberge.11@gmail.com>
2024-08-15 18:37:55 -0400

    Converting to PNGs

--- images_converter_nodes.py
+++ images_converter_nodes.py
@@ -60,20 +60,26 @@ class ImagesToPNG:
         return validate_load_images(directory)
 
     def convert_images_to_png(self, directory: str, output_directory: str, **kwargs):
+        converted_images_paths = list()
         images_paths = list_images_paths(directory)
         images_total = len(images_paths)
         pbar = ProgressBar(images_total)
-        logger.debug(f"Images to convert ({images_total}):\n{', '.join(images_paths)}")
+        logger.debug(f"Images to convert: {images_total}")
         for k, image_path in enumerate(images_paths):
             try:
+                image_name = pathlib.Path(image_path).stem
                 image = Image.open(image_path)
+                save_path = f"{os.path.join(output_directory, image_name)}.png"
+                logger.debug(f"Saving: {save_path}")
+                image.save(save_path, "PNG")
+                converted_images_paths.append(save_path)
             except Exception as e:
                 logger.error(f"An error occured during the convertion of image {image_path}: {e}")
             pbar.update_absolute(k, images_total)
         if pbar is not None:
             pbar.update_absolute(images_total, images_total)
         logger.info(f"Finished converting {images_total} images to PNG")
-        return ("", "", images_total)
+        return ("\n".join(images_paths), "\n".join(converted_images_paths), images_total)
 
 
 NODE_CLASS_MAPPINGS = {
