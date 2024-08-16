198a49ccd9d37034124fffdd4fbb3b7d03506b41
Dominic Roberge <dominic.roberge.11@gmail.com>
2024-08-16 09:38:25 -0400

    ShowString node

--- __init__.py
+++ __init__.py
@@ -1,9 +1,18 @@
 from .images_converter_nodes import ImagesToPNG
 from .text_nodes import ShowString
 
+from .logger import logger
+
 NODE_CLASS_MAPPINGS = {
     "ImagesToPNG": ImagesToPNG,
-    "ShowString": ShowString
+    "ShowString": ShowString,
 }
 
-__all__ = ["NODE_CLASS_MAPPINGS"]
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ImagesToPNG": "Images to PNG converter",
+    "ShowString": "Show String",
+}
+
+logger.info("Loading D00MYs...")
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
