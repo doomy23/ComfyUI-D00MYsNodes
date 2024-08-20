c057472f566ceaa56da5d877f13907e34b89bfaa
Dominic Roberge <dominic.roberge.11@gmail.com>
2024-08-20 11:11:17 -0400

    Continuing...

--- web/js/index.js
+++
@@ -1,4 +0,0 @@
-import "./jquery.js";
-import "./anypalette.js";
-import "./colorbox/jquery.colorbox-min.js"
-import "./jspaint/sync-package.js"
---
+++ web/js/index.ts
@@ -0,0 +1,4 @@
+import "./jspain/lib/jquery-3.4.1.min"
+import "./anypalette"
+import "./colorbox/jquery.colorbox-min"
+import "./jspaint/sync-package"
